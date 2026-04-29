d44e58342725948116c1dfa485123bedcca94401
Ethan <128787335+ChippsEthan@users.noreply.github.com>
2026-04-29 22:01:27 +0200

    added tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,11 @@ dependencies {
 
     // Coil for image loading
     implementation("io.coil-kt:coil-compose:2.5.0")
+
+    // Unit tests
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 kapt {
